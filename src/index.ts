8d576dc796d8d90b4de4bb93d79696c07da4feb7
Vadim Samarin <dezivert@gmail.com>
2019-10-05 22:09:35 +0300

    upd

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,29 @@ import { Server, Data } from 'ws';
 
 const wss = new Server({ port: 3377 });
 
+const fillZeros = (s: string) => s.length === 1 ? '0' + s : s;
+
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message: Data) {
     if (message instanceof Buffer) {
       console.log(`It's a Buffer yo`);
 
-      const data = new Uint8Array(message).map(x => parseInt('' + x, 16)).join(' ');
+      const data = Array.from(new Uint8Array(message))
+                      .map(x => x.toString(16))
+                      .map(fillZeros)
+                      .join(' ');
+
       console.log(data);
-      
+
       this.send(data);
+      this.send(message);
+    } else {
+      console.log('What the heck is that he sending me?:');
+      console.log(message);
     }
   });
 
   console.log('yoba connected');
   ws.send('Welcome to Pornhub');
-  ws.send(`Here's your favorite video :-)`);
-  ws.send(new Buffer('Kappa'));
+  ws.send(`Here's your favorite video ;-)`);
 });
